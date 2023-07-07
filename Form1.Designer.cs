ef5294afd685bb3db9991c96495b3b23130e6c3d
Kamil Tele <szlyrda@gmail.com>
2023-07-07 09:54:40 +0200

    Adding all files

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.saveFile = new System.Windows.Forms.Button();
+            this.loadFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.sourceLabel1 = new System.Windows.Forms.Label();
+            this.destinationLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.symetricCipherButton = new System.Windows.Forms.Button();
+            this.symetricDecipherButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 81);
+            this.textBox1.MaximumSize = new System.Drawing.Size(300, 300);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 300);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(488, 81);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 300);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // saveFile
+            // 
+            this.saveFile.Location = new System.Drawing.Point(594, 400);
+            this.saveFile.Name = "saveFile";
+            this.saveFile.Size = new System.Drawing.Size(75, 23);
+            this.saveFile.TabIndex = 5;
+            this.saveFile.Text = "Zapisz plik";
+            this.saveFile.UseVisualStyleBackColor = true;
+            this.saveFile.Click += new System.EventHandler(this.saveFile_Click);
+            // 
+            // loadFile
+            // 
+            this.loadFile.Location = new System.Drawing.Point(104, 400);
+            this.loadFile.Name = "loadFile";
+            this.loadFile.Size = new System.Drawing.Size(113, 23);
+            this.loadFile.TabIndex = 7;
+            this.loadFile.Text = "Wczytaj plik";
+            this.loadFile.UseVisualStyleBackColor = true;
+            this.loadFile.Click += new System.EventHandler(this.loadFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // sourceLabel1
+            // 
+            this.sourceLabel1.AutoSize = true;
+            this.sourceLabel1.Location = new System.Drawing.Point(105, 446);
+            this.sourceLabel1.Name = "sourceLabel1";
+            this.sourceLabel1.Size = new System.Drawing.Size(12, 15);
+            this.sourceLabel1.TabIndex = 9;
+            this.sourceLabel1.Text = "?";
+            this.sourceLabel1.Click += new System.EventHandler(this.sourceLabel1_Click);
+            // 
+            // destinationLabel
+            // 
+            this.destinationLabel.AutoSize = true;
+            this.destinationLabel.Location = new System.Drawing.Point(105, 472);
+            this.destinationLabel.Name = "destinationLabel";
+            this.destinationLabel.Size = new System.Drawing.Size(12, 15);
+            this.destinationLabel.TabIndex = 10;
+            this.destinationLabel.Text = "?";
+            this.destinationLabel.Click += new System.EventHandler(this.destinationLabel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 446);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Źródło:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 472);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Zapis:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Szyfruj Cezara";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.scipherFileButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 39);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Deszyfruj Cezara";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.descipherFileButton_Click);
+            // 
+            // symetricCipherButton
+            // 
+            this.symetricCipherButton.Location = new System.Drawing.Point(363, 247);
+            this.symetricCipherButton.Name = "symetricCipherButton";
+            this.symetricCipherButton.Size = new System.Drawing.Size(96, 38);
+            this.symetricCipherButton.TabIndex = 14;
+            this.symetricCipherButton.Text = "Szyfruj asymetrycznie";
+            this.symetricCipherButton.UseVisualStyleBackColor = true;
+            this.symetricCipherButton.Click += new System.EventHandler(this.asymetricCipherButton_Click);
+            // 
+            // symetricDecipherButton
+            // 
+            this.symetricDecipherButton.Location = new System.Drawing.Point(363, 291);
+            this.symetricDecipherButton.Name = "symetricDecipherButton";
+            this.symetricDecipherButton.Size = new System.Drawing.Size(96, 38);
+            this.symetricDecipherButton.TabIndex = 15;
+            this.symetricDecipherButton.Text = "Deszyfruj asymetrycznie";
+            this.symetricDecipherButton.UseVisualStyleBackColor = true;
+            this.symetricDecipherButton.Click += new System.EventHandler(this.asymetricDecipherButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 509);
+            this.Controls.Add(this.symetricDecipherButton);
+            this.Controls.Add(this.symetricCipherButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.destinationLabel);
+            this.Controls.Add(this.sourceLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.loadFile);
+            this.Controls.Add(this.saveFile);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button saveFile;
+        private Button loadFile;
+        private OpenFileDialog openFileDialog1;
+        private Label sourceLabel1;
+        private Label destinationLabel;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Button symetricCipherButton;
+        private Button symetricDecipherButton;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace WinFormsApp1
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+          
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        // Metoda obsługi przycisku "Wczytaj plik"
+        private void loadFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Pobierz ścieżkę wybranego pliku
+                sourceLabel1.Text = openFileDialog1.FileName;
+
+                // Odczytaj zawartość pliku do strumienia
+                var fileStream = openFileDialog1.OpenFile();
+
+                // Odczytaj strumień za pomocą obiektu StreamReader i wyświetl go w polu tekstowym textBox1
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    textBox1.Text = reader.ReadToEnd();
+                }
+
+                // Wyczyść pole tekstowe textBox2
+                textBox2.Text = "";
+            }
+        }
+
+        // Metoda obsługi przycisku "Zapisz plik"
+        private void saveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Zapisz zawartość pola tekstowego textBox2 do pliku
+                System.IO.File.WriteAllText(saveFileDialog1.FileName, textBox2.Text);
+
+                // Wyświetl ścieżkę zapisanego pliku
+                destinationLabel.Text = saveFileDialog1.FileName;
+            }
+        }
+
+        // Metoda obsługi przycisku "Szyfruj plik" szyfrem Cezara
+        private void scipherFileButton_Click(object sender, EventArgs e)
+        {
+            
+            int encryption, key = 3; // Liczba klucza szyfrującego, można ją zmienić
+            string userSendsText, encryptedText = "";
+            userSendsText = textBox1.Text;
+
+            // Szyfruj każdy znak w tekście odczytanym z pola textBox1
+            for (int i = 0; i < userSendsText.Length; i++)
+            {
+                int txtUser = (int)userSendsText[i];
+                encryption = txtUser + key; // Przesuń numer znaku o określoną liczbę pozycji na podstawie klucza
+                
+                // Przekonwertuj zaszyfrowany znak na znak Unicode i dodaj go do zaszyfrowanego tekstu
+                encryptedText += Char.ConvertFromUtf32(encryption); 
+            }
+
+            // Wyświetl zaszyfrowany tekst w polu textBox2
+            textBox2.Text = encryptedText;
+            
+        }
+
+        // Metoda do deszyfrowania tekstu za pomocą algorytmu przesuwającego
+        private void descipherFileButton_Click(object sender, EventArgs e)
+        {
+            int decryption;
+            string decryptText = "";
+            int key = 3; // Liczba klucza szyfrującego, można ją zmienić
+            string encryptedText;
+
+            encryptedText = textBox1.Text;
+
+            // Pętla po każdym znaku w zaszyfrowanym tekście
+            for (int i = 0; i < encryptedText.Length; i++)
+            {
+                int encrypted = (int)encryptedText[i];
+                decryption = encrypted - key;
+                decryptText += Char.ConvertFromUtf32(decryption);
+            }
+
+            textBox2.Text = decryptText;
+        }
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void destinationLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sourceLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //netoda szyfrująca
+        static private byte[] EncryptRSA(byte[] userSendsText, CspParameters csp)
+        {
+            using (var RSA = new RSACryptoServiceProvider(csp))
+            {
+                return RSA.Encrypt(userSendsText, false);
+            }
+        }
+
+        //metoda deszyfrująca
+        static private byte[] DecryptRSA(byte[] userSendsText, CspParameters csp)
+        {
+            using (var RSA = new RSACryptoServiceProvider(csp))
+            {
+                return RSA.Decrypt(userSendsText, false);
+            }
+        }
+
+        // Metoda do szyfrowania tekstu asymetrycznego za pomocą algorytmu RSA
+        private void asymetricCipherButton_Click(object sender, EventArgs e)
+        {
+            string userSendsText = textBox1.Text;
+
+            //kontener, w którym będą zapisywane utworzone klucze RSA
+            var csp = new CspParameters
+            {
+                KeyContainerName = "KontenerRSA"
+            };
+
+            byte[] encryptedMsg = EncryptRSA(Encoding.UTF8.GetBytes(userSendsText), csp);
+
+            textBox2.Text = Convert.ToBase64String(encryptedMsg); // zamiana na Base64
+
+        }
+
+        // Metoda do deszyfrowania tekstu asymetrycznego za pomocą algorytmu RSA
+        private void asymetricDecipherButton_Click(object sender, EventArgs e)
+        {
+            string userSendsText = textBox1.Text;
+
+            //kontener, w którym będą zapisywane utworzone klucze RSA
+            var csp = new CspParameters
+            {
+                KeyContainerName = "KontenerRSA"
+            };
+
+            byte[] encryptedMsg = Convert.FromBase64String(userSendsText); // dekodowanie z Base64
+
+            byte[] decryptedMsg = DecryptRSA(encryptedMsg, csp);
+
+            textBox2.Text = Encoding.UTF8.GetString(decryptedMsg);
+        }
+
+
+    }
+}
